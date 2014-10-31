cbbe736afd93983570a09db1eaee7c56497d2927
Jon Guan <jon@scanadu.com>
2014-10-30 18:52:09 -0700

    add prelim client code

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,17 +1,49 @@
 package edu.sjsu.cmpe.cache.client;
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
 public class Client {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
+        
+        CacheServiceInterface cache0 = new DistributedCacheService("http://localhost:3000");
+        CacheServiceInterface cache1 = new DistributedCacheService("http://localhost:3001");
+        CacheServiceInterface cache2 = new DistributedCacheService("http://localhost:3002");
+        
+        List<CacheServiceInterface> servers = new ArrayList(3);
+        servers.add(cache0);
+        servers.add(cache1);
+        servers.add(cache2);
+        
+        
+        ConsistentHash hash = new ConsistentHash(1, servers);
+        
+        for(Object server : servers) {
+            hash.add(server);
+        }
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
-
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
+        char baseChar = 'a';
+        
+        // Create dictionary
+        for(int i = 1; i < 11; i++) {
+            // Get the server
+            CacheServiceInterface server = (CacheServiceInterface)hash.get(Integer.toString(i));
+            
+            if (server == null) {
+                System.out.println("server is NULL!!!!");
+                return;
+            }
+            
+            // Put key and value in server
+            String value = Character.toString(++baseChar);
+            System.out.println("value insert is " + value);
+            server.put(i, value);
+            
+        }
+        
 
         System.out.println("Existing Cache Client...");
     }
